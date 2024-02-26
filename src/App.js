d9046ab21b6691a56142e9092e4870e661175b10
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-02-26 17:43:05 +1100

    Search API's working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import { map } from 'lodash';
+import React, {useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); //after fetching set to false
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        // hits is anm array of 20 images and put them into our image state
+        setImages(data.hits); 
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term])
+
   return (
-    <div className="App">
-      <h1 className="text-6xl">Hello World</h1>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+
+      {isLoading ? 
+        <h1 className="text-6xl text-center mx-auto mt-32">
+          Images are being loaded
+        </h1> : 
+        <div className="grid grid-cols-3 gap-4">
+          {/* mag images */}
+          {images.map(image => (
+            <ImageCard key={image.id} image={image}/>
+          ))}
+        </div>}
     </div>
   );
 }
